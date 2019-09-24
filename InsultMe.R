b013dd633f7ce4e4ceee50cde3e17abe99b8a013
Vsevolod Suschevskiy <50490206+vvseva@users.noreply.github.com>
2019-09-25 00:18:35 +0200

    Update InsultMe.R

--- InsultMe.R
+++ InsultMe.R
@@ -32,26 +32,25 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
-    # Return the requested dataset ----
-    # LanguageInput <- reactive({
-    #     switch(input$language,
-    #            "General" = "En",
-    #            "Glorius" = "Du")
-    # })
-    
     observe({
         if (input$language == "General" ){
-            names <- c(rep("Seva", 20), rep("Andre", 20), rep("Christina", 30), rep("Nastya", 30), rep("Putin", 1))
-            insult <- read_html("http://www.insult.wiki/wiki/Insult_List")
-            insults <- insult %>% html_nodes("ol a") %>% html_attr("title") %>% substring(4)
+            names <- c(rep("Seva", 20), rep("Andre", 20), rep("Christina", 30), 
+                       rep("Nastya", 30), rep("Putin", 1))
+            
+            insults <- read_html("http://www.insult.wiki/wiki/Insult_List") %>% html_nodes("ol a") %>% html_attr("title") %>% substring(4)
+            
         } else {
-            insult <- read_html("http://www.insult.wiki/wiki/Schimpfwort-Liste")
-            insults <- insult %>% html_nodes("ol a") %>% html_attr("title") %>% substring(4)
-            names <- str_c("Der", names,sep = " ")
+            names <- c(rep("Seva", 20), rep("Andre", 20), rep("Christina", 30), 
+                       rep("Nastya", 30), rep("Putin", 1))
+            
+            insults <- read_html("http://www.insult.wiki/wiki/Schimpfwort-Liste") %>% html_nodes("ol a") %>% html_attr("title") %>% substring(4)
+            
+            names <- stringr::str_c("Der", names, sep = " ")
         }
         
         observeEvent(input$do, {
-            output$insult1 <-  renderText(str_c(sample(insults, 1), sample(names, 1), sep = " "))
+            output$insult1 <-  renderText(str_c(sample(insults, 1), 
+                                                sample(names, 1), sep = " "))
         })
     })
 
